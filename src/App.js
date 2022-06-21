9717455c68117e7251ebe68f37d62cd78216adcc
shahin shahi <shahin.shahi1986@gmail.com>
2022-06-21 07:44:43 +0300

    upload simple cripto project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import Coin from './Coin';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=100&sparkline=false')
+      .then(res => {
+        setData(res.data)
+        console.log(res.data, 'data is here')
+      }).catch(err => (console.log(err)))
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+  const filteredCoin = data.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search a currency"
+            className='coin-input'
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoin.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        )
+      })
+      }
     </div>
   );
 }
